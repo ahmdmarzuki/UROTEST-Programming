47d757070fe6050cd682fcd85971a9b6175a46d4
AHMAD MARZUKI <ahmadmarzuki303@gmail.com>
2024-10-08 18:25:33 +0700

    a star final

--- Soal_4_Ahmad Marzuki_16824316_Cakru17/Jawaban_4_Bonus/a_star.py
+++ Soal_4_Ahmad Marzuki_16824316_Cakru17/Jawaban_4_Bonus/a_star.py
@@ -57,10 +57,10 @@ class Spot():
         self.color = ORANGE
     
     def make_closed(self):
-        self.color = RED
+        self.color = GREY
     
     def make_open(self):
-        self.color = GREEN
+        self.color = GREY
     
     def make_barrier(self):
         self.color = BLACK
@@ -91,7 +91,7 @@ class Spot():
             self.neighbors.append(grid[self.row][self.col + 1])
         # LEFT
         if self.col > 0 and not grid[self.row][self.col - 1].is_barrier():
-            self.neighbors.append(grid[self.row+1][self.col - 1])
+            self.neighbors.append(grid[self.row][self.col - 1])
     
     def __lt__ (self, other):
         return False
@@ -103,10 +103,58 @@ def h(p1, p2):
     x2, y2 = p2
     return abs(x1 - x2) + abs(y1 - y2)
 
+def reconstruct_path(came_from, current, draw):
+    while current in came_from:
+        current = came_from[current]
+        current.make_path()
+        draw()
+
 def algorithm(draw, grid, start, end):
-    coutn = 0
-    open_set = PriorityQueue
-    open_set.put(())
+    count = 0
+    open_set = PriorityQueue()
+    open_set.put((0, count, start))
+    came_from = {}
+    g_score = {spot : float("inf") for row in grid for spot in row}
+    g_score[start] = 0
+    f_score = {spot : float("inf") for row in grid for spot in row}
+    f_score[start] = h(start.get_pos(), end.get_pos())
+    
+    open_set_hash = {start}
+    
+    while not open_set.empty():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                
+        current = open_set.get()[2]
+        open_set_hash.remove(current)
+        
+        # Jika sudah mencapai tujuan
+        if current == end:
+            reconstruct_path(came_from, end, draw)
+            end.make_path()
+            return True
+        
+        for neighbor in current.neighbors:
+            temp_g_score = g_score[current] + 1
+            
+            if temp_g_score < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = temp_g_score
+                f_score[neighbor] = temp_g_score + h(neighbor.get_pos(), end.get_pos())
+                if neighbor not in open_set_hash:
+                    count += 1
+                    open_set.put((f_score[neighbor], count, neighbor))
+                    open_set_hash.add(neighbor)
+                    neighbor.make_open()  # Tetangga terbuka -> warna hijau
+                    
+        draw()  # Perbarui tampilan setiap kali grid berubah
+        
+        if current != start:
+            current.make_closed()  # Node yang sudah diproses -> warna merah
+            
+    return False
+
     
 
 
@@ -211,13 +259,20 @@ def main(windows, width):
                     end = None
                     
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and not started:
+                if event.key == pygame.K_SPACE and start and end:
+                    print("clicked")
+                    started = True
                     for row in grid:
                         for spot in row:
-                            spot.update_neighbors()
+                            spot.update_neighbors(grid)
                             
                     algorithm(lambda: draw(windows, grid, ROWS, width), grid, start, end)
+                    started = False
                     
+                if event.key == pygame.K_c:
+                    start = None
+                    end = None
+                    grid = make_grid(ROWS, width)
                 
     pygame.QUIT
 
